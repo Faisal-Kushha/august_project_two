6e855692787afeb3e63b67b1258d9bc59393ffc1
Faisal <fkushha89@hotmail.com>
2023-09-24 18:11:29 +0300

    Adding the 2nd option

--- cypress/e2e/myTest.cy.js
+++ cypress/e2e/myTest.cy.js
@@ -1,9 +1,8 @@
 /// <reference types= "cypress" />
 
-
-cy.on("uncaught:exception", (e) => {
-  return false;
-});
+// cy.on("uncaught:exception", (e) => {
+//   return false;
+// });
 
 describe("template spec", () => {
   it("passes", () => {
@@ -13,19 +12,21 @@ describe("template spec", () => {
     ];
 
     let RandomIndex = Math.floor(Math.random() * websites.length);
-
+    cy.wait(1000);
     cy.visit(websites[RandomIndex]);
 
     cy.get(".cta__saudi").click();
 
     cy.get("#uncontrolled-tab-example-tab-hotels").click();
 
-    if (RandomIndex==0) {
-      cy.get('[data-testid="AutoCompleteInput"]').type("دبي")
-      
-    }else if (RandomIndex==1){
-      cy.get('[data-testid="AutoCompleteInput"]').type("dubai")
-
+    if (RandomIndex == 0) {
+      cy.get('[data-testid="AutoCompleteInput"]').type("دبي");
+    } else if (RandomIndex == 1) {
+      cy.get('[data-testid="AutoCompleteInput"]').type("dubai");
     }
+
+    cy.wait(1000);
+
+    cy.get('[data-testid="AutoCompleteResultItem1"]').click();
   });
 });
